4e2640c4b4a90ba3742a74b6e568c11af87d92a2
Mike <izabella.cristine@nave.org.br>
2026-07-31 09:04:06 -0300

    Enhance Atv01UmItem with styling and fetch logic
    Added styling and improved the fetch function to display activity status.

--- src/components/Atv01UmItem.jsx
+++ src/components/Atv01UmItem.jsx
@@ -24,18 +24,59 @@
   */
 
 
-import { View, Pressable, Text } from 'react-native'
+import { View, Pressable, Text, StyleSheet } from 'react-native'
 import { useState } from 'react'
+
+const estilo = StyleSheet.create({
+  view: {
+    backgroundColor: "rgba(220, 221, 170, 1)",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "20px",
+  },
+  botao: {
+
+  }
+})
 
 export default function Atv01UmItem({}){
   const [resposta, setResposta] = useState(null)
 
+  async function carregarAtv() {
+    await fetch(
+      'https://jsonplaceholder.typicode.com/todos/1',
+      { method: 'GET'}
+    )
+    .then((info) => {
+      console.log(info)
+      return info.json();
+    })
+    .then((resultado) => {
+      console.log(resultado)
 
+      const id = resultado.id;
+      const title = resultado.title;
+      let status = ""
+      if(resultado.completed){
+        status = "feito"
+      } else {
+        status = "a fazer"
+      }
+
+      const usuario = <View>
+        <Text> {id} - {title}: {status} </Text>
+      </View>
+
+      setResposta(usuario)
+    })
+  }
   
   return(
-    <View>
-      <Pressable>
-        Clique abaixo para carregar uma atividade
+    <View style = {estilo.view}>
+      <Pressable onPress={() => carregarAtv()} style = {estilo.botao}>
+        <Text> 
+          Clique abaixo para carregar uma atividade
+        </Text>
       </Pressable>
       {resposta}
     </View>
