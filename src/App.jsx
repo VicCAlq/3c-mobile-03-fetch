ebb9ae0cca67837e6a20848fdaf7318fed8f0e5b
Mike <izabella.cristine@nave.org.br>
2026-08-03 17:08:43 +0000

    Atv 3 feita

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Exemplo02Tratamento from './components/Exemplo02TratamentoSemComentarios'
 import Exemplo03VariosItens from './components/Exemplo03VariosItensSemComentarios';
 import Atv01UmItem from './components/Atv01UmItem';
 import Atv02MuitosItens from './components/Atv02MuitosItens';
+import Atv03TratarErrosDeUm from './components/Atv03TratarErrosDeUm';
+import Atv04TratarErrosDeMuitos from './components/Atv04TratarErrosDeMuitos';
 
 export default function App() {
   return (
@@ -16,6 +18,8 @@ export default function App() {
         <StatusBar style="auto" />
         <Atv01UmItem />
         <Atv02MuitosItens />
+        <Atv03TratarErrosDeUm/>
+        <Atv04TratarErrosDeMuitos/>
       </ScrollView>
     </View>
   );
--- src/components/Atv03TratarErrosDeUm.jsx
+++ src/components/Atv03TratarErrosDeUm.jsx
@@ -27,3 +27,69 @@
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
   */
+
+import { View, Pressable, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+
+export default function Atv03TratarErrosDeUm({}){
+  const [comments, setComments] = useState(null);
+
+  const estilo = StyleSheet.create({
+    view: {
+    borderRadius: "10px",
+    backgroundColor: "#e65ba0",
+    padding: "10px",
+    margin: "20px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+  dados: {
+    backgroundColor: "rgb(253, 133, 223)",
+    padding: "5px",
+    borderRadius: "5px",
+    margin: "10px",
+  },
+  })
+
+  async function ReceberComentario(params) {
+    fetch(
+      'https://jsonplaceholder.typicode.com/comments/20',
+      { method: 'GET' }
+    ) 
+    .then((info)=>{
+      if(!info.ok){
+         throw new Error(`Erro na requisição! Status: ${resposta.status}`);
+      }
+      return info.json();
+    })
+    .then((resultado)=>{
+      const postId = resultado.postId;
+      const id = resultado.id;
+      const email = resultado.email;
+      const name = resultado.name;
+      const body = resultado.body;
+
+      const mensagem = <View style = {estilo.dados}> 
+        <Text>{postId}: {id} - {email}</Text>
+          <Text>{name}</Text>
+          <Text>{body}</Text>
+      </View>
+
+      setComments(mensagem);
+    })
+    .catch((erro)=>{
+      console.log(erro)
+    })
+  }
+
+  return <View style = {estilo.view}>
+    <Pressable onPress={()=>ReceberComentario()} style={estilo.botao}>
+      Clique abaixo para carregar uma atividade
+    </Pressable>
+    {comments}
+  </View>
+}
