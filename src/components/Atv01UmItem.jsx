a171fd6649f63009194ba0c1e0d5058bdeafcd47
Elyson <elyson.franca@nave.org.br>
2026-08-26 07:26:31 -0300

    Atividade:  o senhor não deu o convite pra essa atv pra ela e por isso enviei

--- src/components/Atv01UmItem.jsx
+++ src/components/Atv01UmItem.jsx
@@ -22,3 +22,77 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
+
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { useState } from 'react'
+
+import {useState} from "react";
+import {View, Pressable, Text, } from "react-native";
+
+
+
+const [result, setResult] = useState(<Text>CLiQUE</Text>)
+
+
+
+export default function Atv01UmItem() {
+async function puxarRequisicao() {
+  await fetch(
+    'https://jsonplaceholder.typicode.com/todos/1',
+    {method: 'GET',}
+  )
+  .then((result)=>{
+    console.log(result)
+    return result.json()
+  })
+
+  const respostaStatus = ""
+  if (result.status == true) {
+    const respostaStatus = "feito"
+  }
+  if (result.status == false) {
+     const respostaStatus = "a fazer"
+  }
+
+  const dados = <View>
+    <Text> id = {result.id} </Text>
+    <Text> título = {result.title} </Text>
+    <Text> status = {respostaStatus} </Text>
+  </View>
+
+setResult(dados)
+}
+
+
+  return (
+    <View>
+      <View>
+        <Pressable onPress={() => puxarRequisicao()}>
+          <Text> Clique abaixo para carregar uma atividade </Text>
+        </Pressable>
+          <View>{result}</View>
+      </View>
+    </View>
+  )
+}
+
+
+export default function AtvUmItem() {
+  const [atividade, setAtividade] = useState({});
+
+  function carregar( {
+      fetch(https:jsonplaceholder.typicode.com/todos/1)
+        then((resposta) => resposta.json())
+        then((dados) => setAtividade(dados));
+  })
+
+  retun (
+<view>
+ <Pressable>
+   <text>Clique abaixo para carregar uma atividade</text>
+  </Pressable>
+    {atividade.id} - {atividade.title}:{""}
+    {atividade.completed ? "feito" : "fazendu"}
+ </view>
+  );
+}
